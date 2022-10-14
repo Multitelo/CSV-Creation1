f2f7b8188d4a460d50b5c906acf22c1d6b239ade
Multitelo <emenikeaugustine301@gmail.com>
2022-10-14 16:59:01 +0100

    Writing to a  CSV file using PHP

---
+++ user_data.php
@@ -0,0 +1,23 @@
+<?php
+
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $date = $_POST['date'];
+    $gender = $_POST['gender'];
+    $country = $_POST['country'];
+
+    $f = fopen('userdata.csv', 'a') or die("Could not open or create file!");
+
+    $data = array(
+        $fullname, $email, $date, $gender, $country
+    );
+
+    fputcsv($f, $data);
+
+    fclose($f);
+    
+    echo('<hr>');
+	echo('<pre>');
+	print_r($data);
+	echo('</pre>');
+?>
